02b30ced3a21492c6adc9638dc30791307330862
GaGuarGo <gabrielguarnierigomes@gmail.com>
2025-10-15 16:48:15 -0300

    feat: Enhance app structure with splash and products pages, add error handling, and update analysis options

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:caveo_gaguargo/app/app_widget.dart';
 import 'package:caveo_gaguargo/presentation/pages/error/error_page.dart';
 import 'package:caveo_gaguargo/shared/environment.dart';
@@ -6,20 +9,32 @@ import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  await runZonedGuarded(
+    () async {
+      WidgetsFlutterBinding.ensureInitialized();
 
-  ErrorWidget.builder = (FlutterErrorDetails details) {
-    if (kDebugMode) {
-      return ErrorWidget(details.exception);
-    }
-    return const ErrorPage(
-      title: "Erro inesperado",
-      message: "Algo deu errado. Tente novamente mais tarde.",
-    );
-  };
+      ErrorWidget.builder = (FlutterErrorDetails details) {
+        if (kDebugMode) {
+          return ErrorWidget(details.exception);
+        }
+        return const ErrorPage(
+          title: "Erro inesperado",
+          message: "Algo deu errado. Tente novamente mais tarde.",
+        );
+      };
 
-  await dotenv.load(fileName: ".env");
+      await dotenv.load(fileName: ".env");
 
-  Environment.load();
-  runApp(const AppWidget());
+      Environment.load();
+      runApp(const AppWidget());
+    },
+    (error, stackTrace) {
+      log(
+        'Erro não capturado',
+        error: error,
+        stackTrace: stackTrace,
+        name: 'MAIN',
+      );
+    },
+  );
 }
--- lib/shared/router.dart
+++ lib/shared/router.dart
@@ -1,4 +1,5 @@
 import 'package:caveo_gaguargo/presentation/pages/error/error_page.dart';
+import 'package:caveo_gaguargo/presentation/pages/products/products_page.dart';
 import 'package:caveo_gaguargo/presentation/pages/splash/splash_page.dart';
 import 'package:go_router/go_router.dart';
 
@@ -15,7 +16,16 @@ class AppRouter {
       message: "Não foi possível encontrar a página solicitada.",
     ),
     routes: [
-      GoRoute(path: splash, builder: (context, state) => const SplashPage()),
+      GoRoute(
+        path: splash,
+        name: splash,
+        builder: (context, state) => const SplashPage(),
+      ),
+      GoRoute(
+        path: products,
+        name: products,
+        builder: (context, state) => const ProductsPage(),
+      ),
     ],
   );
 }
