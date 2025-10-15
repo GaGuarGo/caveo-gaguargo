2b09b38ee0ad1e61a69c7917d951ca3f153fb6be
GaGuarGo <gabrielguarnierigomes@gmail.com>
2025-10-15 16:13:25 -0300

    feat: Implement custom error handling with user-friendly error page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:caveo_gaguargo/app/app_widget.dart';
+import 'package:caveo_gaguargo/presentation/pages/error/error_page.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+
+  ErrorWidget.builder = (FlutterErrorDetails details) {
+    if (kDebugMode) {
+      return ErrorWidget(details.exception);
+    }
+    return const ErrorPage(title: "Erro inesperado", message: "Algo deu errado. Tente novamente mais tarde.");
+  };
 
   runApp(const AppWidget());
 }
