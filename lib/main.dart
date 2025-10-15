49d127b8052a93b781feb03b1a3726db3d864d22
GaGuarGo <gabrielguarnierigomes@gmail.com>
2025-10-15 15:53:25 -0300

    feat: Update app icons and add Flutter launcher icons configuration

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,7 @@ import 'package:caveo_gaguargo/app/app_widget.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   runApp(const AppWidget());
 }
