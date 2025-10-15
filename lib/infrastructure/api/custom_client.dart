bc7de82a91f198b609c28207cd47bbbff7d6afb0
GaGuarGo <gabrielguarnierigomes@gmail.com>
2025-10-15 16:31:56 -0300

    feat: Configure base URL for API in CustomClient using Environment instance

--- lib/infrastructure/api/custom_client.dart
+++ lib/infrastructure/api/custom_client.dart
@@ -1,3 +1,4 @@
+import 'package:caveo_gaguargo/shared/environment.dart';
 import 'package:dio/dio.dart';
 import 'package:dio/io.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
@@ -6,7 +7,7 @@ class CustomClient extends DioForNative {
   CustomClient()
     : super(
         BaseOptions(
-          baseUrl: '',
+          baseUrl: Environment.instance.api,
           connectTimeout: const Duration(seconds: 10),
           receiveTimeout: const Duration(seconds: 60),
         ),
--- lib/shared/environment.dart
+++ lib/shared/environment.dart
@@ -2,12 +2,21 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Environment {
   static Environment? _instance;
-  final String api;
-
   Environment._internal({required this.api});
+
+  final String api;
 
   factory Environment.load() {
     _instance ??= Environment._internal(api: dotenv.env['API'] ?? '');
+    return _instance!;
+  }
+
+  static Environment get instance {
+    if (_instance == null) {
+      throw Exception(
+        'Environment não foi inicializado. Chame Environment.load() primeiro.',
+      );
+    }
     return _instance!;
   }
 }
