32367422298f699be806e37d590b082e26ad28c0
GaGuarGo <gabrielguarnierigomes@gmail.com>
2025-10-15 16:03:24 -0300

    feat: Add google_fonts dependency and update theme configuration

--- lib/shared/theme.dart
+++ lib/shared/theme.dart
@@ -1,5 +1,6 @@
 import 'package:caveo_gaguargo/shared/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   static final theme = ThemeData(
@@ -8,8 +9,7 @@ class AppTheme {
     primaryColorLight: kPrimaryColor,
     primaryColorDark: kSecondaryColor,
     scaffoldBackgroundColor: kPrimaryColor,
-    colorScheme: ColorScheme.fromSwatch().copyWith(
-      secondary: kSecondaryColor,
-    ),
+    colorScheme: ColorScheme.fromSwatch().copyWith(secondary: kSecondaryColor),
+    textTheme: GoogleFonts.montserratTextTheme(),
   );
 }
