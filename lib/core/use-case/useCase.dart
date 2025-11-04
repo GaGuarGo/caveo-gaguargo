0b96b90bd8a95da4a4b1a78b6d670c66dea03a61
GaGuarGo <gabrielguarnierigomes@gmail.com>
2025-11-04 10:35:26 -0300

    refactor: Update FetchProductUseCase to use NoParams instead of Null

--- lib/core/use-case/useCase.dart
+++ lib/core/use-case/useCase.dart
@@ -1,5 +1,13 @@
 // ignore_for_file: file_names
 
-abstract class UseCase<R, P> {
+abstract class UseCase<R, P extends Params> {
   Future<R> call({P? params});
+}
+
+abstract class Params {
+  const Params();
+}
+
+abstract class NoParams extends Params {
+  const NoParams();
 }
--- lib/domain/use-cases/products/fetch_products_usecase.dart
+++ lib/domain/use-cases/products/fetch_products_usecase.dart
@@ -2,7 +2,7 @@ import 'package:caveo_gaguargo/core/use-case/useCase.dart';
 import 'package:caveo_gaguargo/domain/entities/product_entity.dart';
 import 'package:caveo_gaguargo/domain/repositories/product_repository.dart';
 
-class FetchProductUseCase implements UseCase<List<ProductEntity>, Null> {
+class FetchProductUseCase implements UseCase<List<ProductEntity>, NoParams> {
   final ProductRepository _repository;
   FetchProductUseCase({required ProductRepository repository})
     : _repository = repository;
